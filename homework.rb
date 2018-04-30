3b199ad6a6335da74a589288eee594df42ebad41
BenCharbonneau <benscharb@gmail.com>
2018-04-30 16:50:49 -0500

    Arrays

--- homework.rb
+++ homework.rb
@@ -1,3 +1,5 @@
+#Hello World
+
 #p "Hello World"
 
 adjective = "Big Bad"
@@ -8,4 +10,34 @@ phrase = "Hello World"
 
 phrase.upcase!
 
-p phrase
+#Nums Array
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+p nums.uniq
+
+p nums.push 0
+
+p nums
+
+p nums.pop
+
+p nums
+
+p nums.length
+
+p nums.include? 8
+
+nums.push 8
+
+p nums.include? 8
+
+#Color Array
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors.sample
+
+p colors.reverse
+
+p colors.map {|x| x.upcase}
