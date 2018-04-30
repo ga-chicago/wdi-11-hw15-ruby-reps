1065ecafea0ab3b1815c640dc254916db1a8e924
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-30 18:12:26 -0500

    Commit 3: Methods

--- homework.rb
+++ homework.rb
@@ -30,3 +30,20 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 p colors.sample
 p colors.reverse
 p colors.map { |a| a.upcase}
+
+# Methods
+
+def find_area height, width
+	height * width
+end
+
+p find_area 4,5
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+def multiply_each_by_five arr 
+	arr.map {|a| a*5 }
+end
+
+p multiply_each_by_five nums
+
