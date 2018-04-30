c4bc26a5e748ee29c2e34426ffc7fd83651fe220
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-30 18:08:51 -0500

    Commit 2: Arrays

--- homework.rb
+++ homework.rb
@@ -8,5 +8,25 @@ p "Hello #{adjective} World"
 
 world = "Hello World"
 
-
 p world.upcase
+
+# ARRAYS
+
+# Num Array
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+p nums.uniq
+
+nums.push(11)
+nums.pop
+nums.length
+
+p nums.include? 8
+
+# Color Array
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors.sample
+p colors.reverse
+p colors.map { |a| a.upcase}
