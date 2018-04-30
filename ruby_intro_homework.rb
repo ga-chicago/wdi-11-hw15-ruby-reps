eb2803ea6724c6a0ec2ff2dc20fadac7e0c57c15
marietrull <marietrull@gmail.com>
2018-04-30 16:56:15 -0500

    Commit 2: Arrays.

--- ruby_intro_homework.rb
+++ ruby_intro_homework.rb
@@ -9,3 +9,26 @@ p "Hello #{adjective} World"
 hello_world = "Hello World"
 
 p hello_world.upcase
+
+# ARRAYS
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+p nums.uniq
+
+p nums.push 44
+
+p nums.pop
+
+p nums.length
+
+p nums.include? 8
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors.sample
+
+p colors.reverse
+
+p colors.map(&:upcase)
+
