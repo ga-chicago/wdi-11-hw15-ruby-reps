efa24275d7e088aabf75a1771d4ec4002d98bbd4
BenCharbonneau <benscharb@gmail.com>
2018-04-30 16:58:27 -0500

    Methods with a Hash

--- homework.rb
+++ homework.rb
@@ -57,3 +57,39 @@ def multiply_each_by_five arr
 end
 
 multiply_each_by_five nums
+
+#Methods with a Hash
+
+# Hashes
+
+book = {
+  title: 'The Great Gatsby',
+  author: 'F Scott Fitzgerald',
+  year: 1925,
+  price: 10
+}
+
+lamp = {
+  type: 'reading',
+  brand: 'Ikea',
+  price: 25
+}
+
+table = {
+  type: 'bed side',
+  brand: 'Crate & Barrel',
+  color: 'birch',
+  price: 50
+}
+
+def print_price hash
+	hash[:price]
+end
+
+p print_price table
+
+def print_item_sums hash1,hash2
+	hash1[:price] + hash2[:price]
+end
+
+p print_item_sums lamp,book
