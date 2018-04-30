8900d6991aac4f47dc253dff12fb70619cd577a7
BenCharbonneau <benscharb@gmail.com>
2018-04-30 16:55:04 -0500

    Methods

--- homework.rb
+++ homework.rb
@@ -40,4 +40,20 @@ p colors.sample
 
 p colors.reverse
 
-p colors.map {|x| x.upcase}
+colors.map {|x| p x.upcase}
+
+#Methods
+
+def find_area height, width
+	height*width
+end
+
+p find_area 4,5
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+def multiply_each_by_five arr
+	arr.map {|x| p x*5}
+end
+
+multiply_each_by_five nums
