cca519689826e19e7f84097feec22e06940c5803
Irwin <irgranados16@gmail.com>
2018-04-30 16:37:47 -0500

    Commit 1: Hello World

---
+++ homework.rb
@@ -0,0 +1,17 @@
+# Hello World
+
+# Print "Hello World" to the console
+
+p 'Hello World'
+
+# After you have printed Hello World:
+
+adjective = "Big Bad"
+
+# Interpolate the adjective variable into the Hello World string using #{}
+
+p "Hello #{adjective} World"
+
+# Save "Hello World" to a variable. Without changing the letters in your code manually, permanently change "Hello World" to uppercased letters.
+
+variable = (puts("hello world".upcase))
