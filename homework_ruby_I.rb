498ace407553477d1e097dfe156383104e932e9a
Zoe <zoereagan@gmail.com>
2018-04-30 16:49:05 -0500

    commit 2: arrays

--- homework_ruby_I.rb
+++ homework_ruby_I.rb
@@ -2,3 +2,25 @@
 #1. Print "Hello World" to the console
 
 p "Hello, World"
+
+#Arrays
+# Use .uniq to print the array with all duplicate entries removed
+# Next, use .push and .pop and .length on the array as you would with javaScript
+# Use .include? to check if the array contains the number 8
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+
+p nums.uniq
+p nums.push[0]
+p nums.pop[1]
+p nums.length
+
+p nums.include? 8
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors.sample
+p colors.reverse
+
+p colors.to_s.upcase
