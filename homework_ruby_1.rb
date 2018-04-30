664059ff69e09a47c4707d9673bceaa3bfff3e41
Sam <sammintz1993@gmail.com>
2018-04-30 16:46:46 -0500

    Commit 2: Arrays.

--- homework_ruby_1.rb
+++ homework_ruby_1.rb
@@ -13,3 +13,46 @@ p "Hello #{adjective} World"
 world = "Hello World"
 
 p world.upcase
+
+
+# Nums Array
+# With the following array:
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+# Use .uniq to print the array with all duplicate entries removed
+
+p nums.uniq
+
+# Next, use .push and .pop and .length on the array as you would with javaScript
+
+p nums.uniq.push(410)
+
+p nums.uniq.pop
+
+p nums.uniq.length
+
+# Use .include? to check if the array contains the number 8
+
+p nums.include?(8)
+
+#false
+
+# Color Array
+# With the following array:
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+#Print out a random color.
+
+p colors.sample
+
+#Print out the colors array in reverse order.
+
+p colors.reverse
+
+#Print out all of the colors in the array with all capital letters.
+
+# p colors.to_s.upcase
+
+p colors.map!(&:upcase)
+
