187ef642c344faa731cef7a8ebb00e4d1797c900
Sam <sammintz1993@gmail.com>
2018-04-30 17:18:56 -0500

    Commit 5: Euler problem 1.

--- homework_ruby_1.rb
+++ homework_ruby_1.rb
@@ -117,6 +117,23 @@ end
 p print_item_sums table, lamp
 
 
+# PROBLEMS
+# EULER PROBLEM 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiples
+  (1..999).select do |i|
+    i % 3 == 0 || i % 5 == 0
+  end
+end
+
+def sumMultiples(multiples)
+  total = multiples.inject(&:+)
+end
+
+p sumMultiples multiples
 
 
 
