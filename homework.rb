a90df815fac08796067eec5ed797bee4af9078c1
Hannah Werman <hannah.werman@gmail.com>
2018-04-30 16:54:01 -0500

    arrays

--- homework.rb
+++ homework.rb
@@ -9,3 +9,30 @@ p "Hello #{adj} world"
 greeting = "Hello World"
 
 p greeting.upcase
+
+# ---- ARRAYS ------
+# -----NUMS ARRAY -----
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+uniq_nums = nums.uniq
+
+p uniq_nums
+
+uniq_nums.pop
+
+uniq_nums.push(8)
+
+p uniq_nums.length
+
+p uniq_nums.include?(8) #returns true because line 24
+
+# ---- color array -----
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors.sample
+
+p colors.reverse
+
+p colors.map(&:capitalize)
