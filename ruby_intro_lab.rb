8551da4f89dab61c56f69a6a280eeeb8079b7c7c
Noemi Romero <nlll02@yahoo.com>
2018-04-30 21:23:53 -0500

    Commit 2: Arrays

--- ruby_intro_lab.rb
+++ ruby_intro_lab.rb
@@ -97,10 +97,49 @@ adjective = "Big Bad"
 #into the Hello World string using `#{}`
 
 p "Hello #{adjective} World"
-
+# 3. Save "Hello World" to a variable. 
+# Without changing the letters in your code manually,
+#  permanently change "Hello World" to uppercased letters.
 phrase = "Hello World"
 
 p phrase.upcase
+
+## Arrays
+
+### Nums Array
+# With the following array:
+# ```ruby
+ nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+# 1. Use `.uniq` to print the array with all duplicate entries removed
+p nums.uniq
+# 2. Next, use `.push` and `.pop` and `.length` on the array as you would with javaScript
+p nums.push(8)
+p nums.pop()
+p nums.length
+
+# 3. Use `.include?` to check if the array contains the number 8
+
+p nums.include? 8
+p nums
+
+
+### Color Array
+
+# With the following array:
+
+# ```ruby
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+# ```
+
+# 1. Print out a random color.
+
+p random_color = colors.sample
+
+# 2. Print out the `colors` array in reverse order.
+p colors.reverse
+# 3. Print out all of the colors in the array with all capital letters.
+p colors.map(&:upcase)
+
 
 
 
