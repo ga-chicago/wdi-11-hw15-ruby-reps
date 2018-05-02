f8f02c4c6239e72ce840a920635762ce96463e89
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-01 23:55:48 -0500

    Arrays complete

--- hw.rb
+++ hw.rb
@@ -14,4 +14,44 @@ str.upcase!
 p str
 
 
+# Arrays
+
+# Num Array
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+p nums.uniq
+
+nums.push(100)
+
+nums.push(101)
+
+p nums
+
+nums.pop()
+
+p nums
+
+p nums.include? 8
+
+
+
+# Color Array
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+p colors[rand(colors.length)]
+
+p colors.reverse
+
+colors.map { |color| p color.upcase  }
+
+
+
+
+
+
+
+
+
 
