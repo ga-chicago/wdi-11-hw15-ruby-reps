9c8e55012ad6dffeb7c2f0137f8ca5e2b2ee8c3c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-02 00:01:17 -0500

    Method

--- hw.rb
+++ hw.rb
@@ -47,6 +47,28 @@ p colors.reverse
 colors.map { |color| p color.upcase  }
 
 
+# Methods
+
+def find_area width, height
+	width * height
+end
+
+p find_area 4, 4
+
+
+
+
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+
+def multiply_5 array
+	array.map { |num| num * 5  }
+end
+
+p multiply_5 nums		
+
+
 
 
 
